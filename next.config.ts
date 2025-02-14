bf61fbc177fcf7077acb99047b6afad674c8eddb
TrustinN <trustin.nguyen62@berkeley.edu>
2025-02-13 23:22:01 -0800

    ?

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  assetPrefix: "./", // Required for GitHub Pages
+  basePath: "/web2",
+  assetPrefix: "./web2", // Required for GitHub Pages
 };
 
 export default nextConfig;
